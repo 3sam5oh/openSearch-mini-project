39feebaf8a9717df87e11419dc4aeb25b75415b2
jhlee <jun73045928@gmail.com>
2024-06-28 13:12:34 +0900

    init project

---
+++ searchEngineProject/src/main/java/org/samsamohoh/searchengineproject/SearchEngineProjectApplication.java
@@ -0,0 +1,13 @@
+package org.samsamohoh.searchengineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchEngineProjectApplication.class, args);
+    }
+
+}
---
+++ searchEngineProject/src/test/java/org/samsamohoh/searchengineproject/SearchEngineProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samsamohoh.searchengineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEngineProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
