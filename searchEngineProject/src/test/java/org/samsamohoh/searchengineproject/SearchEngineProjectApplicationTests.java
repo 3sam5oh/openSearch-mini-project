fcede35d162cc27093220afaf88aee3e896b321b
JaewonLee <jlee38266@gmail.com>
2024-07-03 18:51:11 +0900

    살려줘

--- searchEngineProject/src/test/java/org/samsamohoh/searchengineproject/SearchEngineProjectApplicationTests.java
+++ searchEngineProject/src/test/java/org/samsamohoh/searchengineproject/SearchEngineProjectApplicationTests.java
@@ -1,13 +1,18 @@
 package org.samsamohoh.searchengineproject;
 
 import org.junit.jupiter.api.Test;
+import org.opensearch.client.RestHighLevelClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class SearchEngineProjectApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @MockBean
+//    private RestHighLevelClient restHighLevelClient;
+//
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
